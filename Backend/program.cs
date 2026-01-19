7592af484945015fc6a96282023bca4b2df6e8a9
csgorun <dimitryi455@gmail.com>
2026-01-19 15:09:51 +0300

    Added basic backend program

---
+++ Backend/program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Слюнин Д.А.");
+Console.WriteLine(DateTime.Now);
