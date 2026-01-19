1f74a6f8367a22a9aadd88ccea233775d8088632
csgorun <dimitryi455@gmail.com>
2026-01-19 15:54:31 +0300

    logic

--- Backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0192db43441e34b318bff612f9a4e2e5f696a2e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41cf6e359ce0d4126c2883a547406cc8b7c2ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/program.cs
+++ Backend/program.cs
@@ -7,3 +7,13 @@ Console.Write("Введите свое имя:");
 string name = Console.ReadLine();
 Console.WriteLine($"Привет, {name}!");
 Console.WriteLine($"Сегодня: {DateTime.Now.DayOfWeek}");
+Console.WriteLine("Хотите продолжить/выйти? (y/n)");
+string next = Console.ReadLine();
+ if (next == "y" || next == "Y")
+        {
+            Console.WriteLine("Вы выбрали: продолжить.");
+        }
+        else if (next == "n" || next == "N")
+        {
+            Console.WriteLine("Вы выбрали: выход.");
+        }
